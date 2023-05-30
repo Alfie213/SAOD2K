298b55ae78e1f0818ec3c7e5f694073c3e4bc873
Alfie213 <arturbogoratov@gmail.com>
2023-05-30 16:58:16 +0300

    Archive (Done)

--- Archive/Archive/Archive.cpp
+++ Archive/Archive/Archive.cpp
@@ -1,161 +1,259 @@
 ﻿#include <iostream>
 #include <fstream>
-#include <sstream>
-#include <unordered_map>
 #include <string>
+#include <vector>
+#include <sstream>
+#include "windows.h"
 
-void compress(const std::string& input, std::string& output) {
-    std::unordered_map<std::string, int> dictionary;
-    for (int i = 0; i < 256; ++i) {
-        dictionary[std::string(1, i)] = i;
-    }
+using namespace std;
 
-    std::string current_string;
-    int next_code = 256;
+struct Node {
+	int index;
+	string data;
+	Node* next;
+};
 
-    for (char c : input) {
-        current_string += c;
-        if (dictionary.find(current_string) == dictionary.end()) {
-            dictionary[current_string] = next_code++;
-            current_string.pop_back();
-            output += std::to_string(dictionary[current_string]) + " ";
-            current_string = c;
-        }
-    }
-
-    if (!current_string.empty()) {
-        output += std::to_string(dictionary[current_string]) + " ";
-    }
+void st_Node(Node* head, int index, string data) {
+	head->index = index;
+	head->data = data;
+	head->next = NULL;
 }
 
-void decompress(const std::string& input, std::string& output) {
-    std::unordered_map<int, std::string> dictionary;
-    for (int i = 0; i < 256; ++i) {
-        dictionary[i] = std::string(1, i);
-    }
+void insert_Node(Node* head, int index, string data) {
+	Node* new_Node = new Node;
+	new_Node->index = index;
+	new_Node->data = data;
+	new_Node->next = NULL;
 
-    int next_code = 256;
-    int code;
-    std::istringstream input_stream(input);
-
-    while (input_stream >> code) {
-        std::string entry = dictionary[code];
-        if (entry.empty()) {
-            entry = dictionary[next_code - 1] + dictionary[next_code - 1][0];
-        }
-
-        output += entry;
-        dictionary[next_code++] = dictionary[next_code - 1] + entry[0];
-    }
+	Node* curr = head;
+	while (curr != NULL)
+	{
+		if (curr->next == NULL)
+		{
+			curr->next = new_Node;
+			return;
+		}
+		curr = curr->next;
+	}
 }
 
-//void decompress(const std::string& input, const std::string& path) {
-//    std::unordered_map<int, std::string> dictionary;
-//    for (int i = 0; i < 256; ++i) {
-//        dictionary[i] = std::string(1, i);
-//    }
-//
-//    int next_code = 256;
-//    int code;
-//    std::istringstream input_stream(input);
-//
-//    std::ofstream outputFile(path);
-//    if (!outputFile.is_open()) {
-//        std::cout << "Failed to open file" << std::endl;
-//        exit(1);
-//    }
-//
-//    while (input_stream >> code) {
-//        std::string entry = dictionary[code];
-//        if (entry.empty()) {
-//            entry = dictionary[next_code - 1] + dictionary[next_code - 1][0];
-//        }
-//
-////        std::cout << "sym " << entry;
-//        outputFile << entry;
-//        dictionary[next_code++] = dictionary[next_code - 1] + entry[0];
-//    }
-//
-//    outputFile.close();
-//}
-
-void checkInput(int argc, char** argv) {
-    if (argc < 2) {
-        std::cout << "Usage:" << std::endl;
-        std::cout << "FILE_INPUT {C or D} NAME_OUTPUT" << std::endl;
-        exit(1);
-    }
+Node* search_Node(Node* head, string data)
+{
+	Node* curr = head;
+	while (curr != NULL)
+	{
+		if (data.compare(curr->data) == 0)
+			return curr;
+		else
+			curr = curr->next;
+	}
+	return NULL;
 }
 
-int main(int argc, char** argv) {
+Node* search_Node(Node* head, int index)
+{
+	Node* curr = head;
+	while (curr != NULL)
+	{
+		if (index == curr->index)
+			return curr;
+		else
+			curr = curr->next;
+	}
+	return NULL;
+}
 
-    checkInput(argc, argv);
+bool delete_Node(Node* head, Node* to_delete) {
+	if (to_delete == NULL)
+		return false;
+	else if (to_delete == head)
+	{
+		head = to_delete->next;
+		delete to_delete;
+		return true;
+	}
+	else {
+		Node* curr = head;
+		while (curr)
+		{
+			if (curr->next == to_delete)
+			{
+				curr->next = to_delete->next;
+				delete to_delete;
+				return true;
+			}
+			curr = curr->next;
+		}
+		return false;
+	}
+}
 
-    std::string inputCommand(argv[2]);
-    
-    if (inputCommand == "C") {
-        std::cout << "Compressing... please wait" << std::endl;
+vector <string> split(string str, char delimiter) {
+	vector<string> internal;
+	stringstream ss(str); // Turn the string into a stream.
+	string tok;
 
-        std::ifstream inputFile(argv[1]);
-        if (!inputFile.is_open()) {
-            std::cout << "Failed to open file" << std::endl;
-            return 1;
-        }
-        std::string input;
-        std::string str;
-        while (std::getline(inputFile, str)) {
-            input += str;
-        }
-        inputFile.close();
+	while (getline(ss, tok, delimiter)) {
+		internal.push_back(tok);
+	}
 
-        //std::cout << "Input: " << input << std::endl;
-        std::string compressed;
+	return internal;
+}
 
-        compress(input, compressed);
-        //std::cout << "Compressed: " << compressed << std::endl;
+string LZ78(string input, int option)
+{
+	if (option == 1)
+	{
+		Node* dictionary = new Node;
+		string word, result;
+		int length, last_seen, index = 1;
 
-        std::string output = argv[3];
-        output += ".myarch";
-        std::ofstream outFile(output);
-        if (!outFile.is_open()) {
-            std::cout << "Failed to open file" << std::endl;
-            return 1;
-        }
-        outFile << compressed;
-        outFile.close();
+		length = (int)input.length();
+		word = input[0];
+		st_Node(dictionary, 1, word);
+		result += "0," + word;
 
-        std::cout << "Done!" << std::endl;
-    }
-    else if (inputCommand == "D") {
-        std::cout << "Decompressing... please wait" << std::endl;
+		for (int i = 1; i < length; i++)
+		{
+			string data;
+			data = input[i];
 
-        std::ifstream inputFile(argv[1]);
-        if (!inputFile.is_open()) {
-            std::cout << "Failed to open file" << std::endl;
-            return 1;
-        }
-        std::string compressed;
-        std::getline(inputFile, compressed);
-        inputFile.close();
+		re_check:
+			Node* search = search_Node(dictionary, data);
 
-        //std::cout << "Compressed: " << compressed << std::endl;
+			if (search)
+			{
+				i++;
+				data += input[i];
+				last_seen = search->index;
+				goto re_check;
+			}
+			else
+			{
+				char zero;
+				if (input[i] == ' ')
+					zero = '0';
+				else
+					zero = input[i];
 
-        std::string decompressed;
-        decompress(compressed, decompressed);
-        //decompress(compressed, argv[3]); For commented variant.
-        //std::cout << "Decompressed: " << decompressed << std::endl;
+				if ((int)data.length() < 2)
+					result += " " + to_string(0) + "," + zero;
+				else
+					result += " " + to_string(last_seen) + "," + zero;
 
-        std::ofstream file(argv[3]);
-        file << decompressed;
-        file.close();
+				index++;
+				if (i != length)
+					insert_Node(dictionary, index, data);
+			}
+		}
 
-        std::cout << "Done!" << std::endl;
-    }
-    else {
-        std::cout << "Usage:" << std::endl;
-        std::cout << "FILE_INPUT {C or D} NAME_OUTPUT" << std::endl;
-        return 1;
-    }
+		return result;
+	}
+	else if (option == 2)
+	{
+		Node* dictionary = new Node;
+		string result;
 
-    return 0;
+		vector <string> s_input = split(input, ' ');
+		int zz = 2;
+		for (int i = 0; i < s_input.size(); i++)
+		{
+			vector <string> ss_input = split(s_input[i], ',');
+
+			if (i == 0)
+			{
+				st_Node(dictionary, 1, ss_input[1]);
+				result += ss_input[1];
+			}
+			else
+			{
+				Node* serched;
+				string get_search = ss_input[1];
+				serched = search_Node(dictionary, stoi(ss_input[0]));
+				if (serched)
+				{
+					result += serched->data + get_search;
+					get_search = serched->data + split(s_input[i], ',')[1];
+					insert_Node(dictionary, zz, get_search);
+				}
+				else
+				{
+					if (stoi(ss_input[0]) == 0)
+						insert_Node(dictionary, zz, get_search);
+					else
+						insert_Node(dictionary, zz, get_search);
+
+					result += get_search;
+				}
+				zz++;
+			}
+		}
+
+		if (result[(int)result.length() - 1] == '0')
+			result = result.substr(0, result.size() - 1);
+
+		return result;
+	}
+	return "";
+}
+
+int main(int argc, char* argv[])
+{
+	/*string input, result, method_text;
+	int method, option, option2;
+
+	std::cout << LZ78("hello123", 1) << std::endl;
+	std::cout << LZ78("0,h 0,e 0,l 3,o 0,1 0,2 0,3", 2) << std::endl;*/
+
+	if (argc != 4) {
+		cout << "Usage: program_name <input_file> <output_file> <option>" << endl;
+		cout << "Options:" << endl;
+		cout << "  1 - LZ78 encoding" << endl;
+		cout << "  2 - LZ78 decoding" << endl;
+		return 1;
+	}
+
+	string input_file = argv[1];
+	string output_file = argv[2];
+	int option = stoi(argv[3]);
+
+	ifstream input_stream(input_file);
+	if (!input_stream) {
+		cout << "Failed to open input file." << endl;
+		return 1;
+	}
+
+	string input;
+	string line;
+	while (getline(input_stream, line))
+	{
+		input += line;
+		input += "\n";
+	}
+	input_stream.close();
+
+	string result;
+	if (option == 1) {
+		result = LZ78(input, 1);
+	}
+	else if (option == 2) {
+		result = LZ78(input, 2);
+	}
+	else {
+		cout << "Invalid option. Please choose 1 for encoding or 2 for decoding." << endl;
+		return 1;
+	}
+
+	ofstream output_stream(output_file);
+	if (!output_stream) {
+		cout << "Failed to open output file." << endl;
+		return 1;
+	}
+
+	output_stream << result;
+	output_stream.close();
+
+	cout << "Operation completed successfully." << endl;
+
+	return 0;
 }
